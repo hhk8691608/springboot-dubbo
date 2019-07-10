d058ccf73f0c000ddf97f714380d6ca9ec462234
hhk8691608 <15625140883@163.com>
2019-07-10 15:50:11 +0800

    结合zk

--- consumer/src/main/java/com/mooc/jiangzh/dubbo/springboot/consumer/quickstart/QuickstartConsumer.java
+++ consumer/src/main/java/com/mooc/jiangzh/dubbo/springboot/consumer/quickstart/QuickstartConsumer.java
@@ -1,6 +1,7 @@
 package com.mooc.jiangzh.dubbo.springboot.consumer.quickstart;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.config.annotation.Service;
 import com.mooc.jiangzh.dubbo.springboot.ServiceAPI;
 
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class QuickstartConsumer {
 
-    @Reference(url="dubbo://localhost:20880")
+    @Reference(interfaceClass = ServiceAPI.class)
     ServiceAPI serviceAPI;
 
     public void sendMessage(String message){
